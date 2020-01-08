9cf297fa026c7a6c4861b19c48e74b8c9eddad39
Shiraaz Moollatjie <mllshi011@gmail.com>
2020-01-08 23:55:59 +0200

    retroachievements: Adding placeholder functions

---
+++ retroachievements.go
@@ -0,0 +1,45 @@
+package retroachievementsgo
+
+func (c *Client) GetTopTenUsers() string {
+	panic("implement me!")
+}
+
+func (c *Client) GetConsoleIDs() string {
+	panic("implement me!")
+}
+
+func (c *Client) GetGameList() string {
+	panic("implement me!")
+}
+
+func (c *Client) GetGameInfo() string {
+	panic("implement me!")
+}
+
+func (c *Client) GetEnhancedGameInfo() string {
+	panic("implement me!")
+}
+
+func (c *Client) GetGameProgress() string {
+	panic("implement me!")
+}
+
+func (c *Client) GetUserRankAndScore() string {
+	panic("implement me!")
+}
+
+func (c *Client) GetUserSummary() string {
+	panic("implement me!")
+}
+
+func (c *Client) GetUserFeed() string {
+	panic("implement me!")
+}
+
+func (c *Client) ListUserAchievementsByDate() string {
+	panic("implement me!")
+}
+
+func (c *Client) ListUserAchievementsByDateRange() string {
+	panic("implement me!")
+}
