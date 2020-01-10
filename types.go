46b306bfc6d47dbb5a8003059393df4413e8b81d
Shiraaz Moollatjie <mllshi011@gmail.com>
2020-01-11 01:22:40 +0200

    types: Adding a RetroInt type

---
+++ types.go
@@ -0,0 +1,32 @@
+package retroachievementsgo
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+)
+
+// RetroInt is a retroachievement type that is a string for a number > 0 or an
+// int when it is zero
+type RetroInt int
+
+func (ri *RetroInt) UnmarshalJSON(data []byte) error {
+	i, err := strconv.Atoi(string(data))
+	if err != nil {
+		// might be a string
+		var str string
+		err := json.Unmarshal(data, &str)
+		if err != nil {
+			return err
+		}
+
+		i, err = strconv.Atoi(str)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("value is %v\n", i)
+	}
+
+	*ri = RetroInt(i)
+	return nil
+}
---
+++ types_test.go
@@ -0,0 +1,36 @@
+package retroachievementsgo
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+type TestPlaceholder struct {
+	Test RetroInt
+}
+
+func TestUnmarshalRetroInt(t *testing.T) {
+	type RetroIntPlaceHolder struct {
+		Value RetroInt
+	}
+
+	testCases := []struct {
+		desc     string
+		json     string
+		expected RetroInt
+	}{
+		{"Basic json", `{"value":"123"}`, RetroInt(123)},
+		{"Test zero", `{"value":0}`, RetroInt(0)},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			var res *RetroIntPlaceHolder
+			json.Unmarshal([]byte(tC.json), &res)
+			fmt.Printf("%+v", res)
+			if res.Value != tC.expected {
+				t.Errorf("Incorrect RetroInt. \nExpected: %v\nActual: %v\n", tC.expected, res.Value)
+			}
+		})
+	}
+}
